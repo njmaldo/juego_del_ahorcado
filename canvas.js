bc36f3ee5c8b7a5988f5a883a2f7df885e695993
nestor Maldonado <nestorjmaldo@gmail.com>
2022-09-29 20:14:40 -0300

    segundo commit

--- canvas.js
+++ canvas.js
@@ -4,10 +4,10 @@ const crearCanvas = () => {
     tablero.lineJoin = 'round';
     tablero.fillStyle = '#F3F5F6';
     tablero.strokeStyle = '#8A3871'
-    tablero.fillRect(0,0,1200,860);
+    tablero.fillRect(0, 0, 1200, 860);
     tablero.beginPath();
-    tablero.moveTo(650,500);
-    tablero.lineTo(900,500);
+    tablero.moveTo(600, 500);
+    tablero.lineTo(900, 500);
     tablero.stroke();
     tablero.closePath();
 }
@@ -16,10 +16,10 @@ const dibujarLineas = () => {
     tablero.lineCap = 'round';
     tablero.lineJoin = 'round';
     tablero.fillStyle = '#F3F5F6';
-    tablero.strokeStyle = '#8A3871'
+    tablero.strokeStyle = '#024252'
 
-    let ancho = 600/palabraSecreta.length;
-    for(let i = 0; i < palabraSecreta.length; i++) {
+    let ancho = 600 / palabraSecreta.length;
+    for (let i = 0; i < palabraSecreta.length; i++) {
         tablero.moveTo(500 + (ancho * i), 640);
         tablero.lineTo(550 + (ancho * i), 640);
     }
@@ -28,14 +28,14 @@ const dibujarLineas = () => {
 }
 
 const letraCorrecta = (index) => {
-    tablero.font = 'bold 52px Inter';
+    tablero.font = ' 52px Inter';
     tablero.lineWidth = 6;
     tablero.lineCap = 'round';
     tablero.lineJoin = 'round';
     tablero.fillStyle = '#F3F5F6';
 
-    let ancho = 600/palabraSecreta.length;
-    tablero.strokeText(palabraSecreta[index], 505+(ancho*index), 620);
+    let ancho = 600 / palabraSecreta.length;
+    tablero.strokeText(palabraSecreta[index], 505 + (ancho * index), 620);
     tablero.stroke();
 }
 
@@ -45,5 +45,77 @@ const letraIncorrecta = (letra, espacio) => {
     tablero.lineCap = 'round';
     tablero.lineJoin = 'round';
     tablero.fillStyle = '#F3F5F6';
-    tablero.strokeText(letra, 535+(40*(10-espacio)), 710, 40);
+    tablero.strokeText(letra, 535 + (40 * (10 - espacio)), 710, 40);
+}
+// resto
+const dibujarAhorcado = (puntaje) => {
+    tablero.lineWidth = 8;
+    tablero.lineCap = 'round';
+    tablero.lineJoin = 'round';
+    tablero.strokeStyle = '#0A3871';
+
+    if (puntaje === 8) {
+        tablero.moveTo(700, 500);
+        tablero.lineTo(700, 100);
+    }
+    if (puntaje === 7) {
+        tablero.moveTo(850, 100);
+        tablero.lineTo(700, 100);
+    }
+    if (puntaje === 6) {
+        tablero.moveTo(850, 100);
+        tablero.lineTo(850, 171);
+    }
+    if (puntaje === 5) {
+        tablero.moveTo(900, 230);
+        tablero.arc(850, 230, 50, 0, Math.PI * 2);
+    }
+    if (puntaje === 4) {
+        tablero.moveTo(850, 389);
+        tablero.lineTo(850, 289);
+    }
+    if (puntaje === 3) {
+        tablero.moveTo(850, 389);
+        tablero.lineTo(800, 450);
+    }
+    if (puntaje === 2) {
+        tablero.moveTo(850, 389);
+        tablero.lineTo(890, 450);
+    }
+    if (puntaje === 1) {
+        tablero.moveTo(850, 330);
+        tablero.lineTo(800, 389);
+    }
+    if (puntaje === 0) {
+        tablero.moveTo(850, 330);
+        tablero.lineTo(890, 389);
+    }
+    tablero.stroke();
+    tablero.closePath();
+}
+
+const perdio = () => {
+    tablero.font = 'bold 42px Inter';
+    tablero.lineWidth = 6;
+    tablero.lineCap = 'round'
+    tablero.lineJoin = 'round';
+    tablero.fillStyle = 'red';
+    tablero.moveTo(850, 330);
+    tablero.fill()
+    tablero.fillText('GAME OVER!', 900, 320);
+}
+
+const gano = () => {
+    tablero.font = 'bold 42px Inter';
+    tablero.lineWidth = 6;
+    tablero.lineCap = 'round';
+    tablero.lineJoin = 'round';
+    tablero.fillStyle = 'green';
+    tablero.fillText('You Win!', 870, 320);
+    tablero.fillText("Congratulations!", 870, 360);
+    setTimeout(recargar, 2000);
+}
+
+const recargar = () => {
+    location.reload();
 }
--- juego.js
+++ juego.js
@@ -1,56 +1,148 @@
 
 //Variables
-let palabras = ['ALURA','ORACLE','ONE','JAVASCRIPT','HTML'];
-let tablero  = document.getElementById('canvas').getContext('2d');
-let palabraSecreta = '';
-let letras = [];
-let errores = 8;
+let btnNuevoJuego = document.getElementById('nuevo-juego').style.display = 'none';
+let txtPalabra = document.getElementById('n-palabra').style.display = 'none';
+let btn5_6 = document.getElementById('button-5-6-content').style.display = 'none';
+let btnSalir = document.getElementById('salir').style.display = 'none';
+let signo = document.getElementById('texto').style.display = 'none';
+let pantalla = document.getElementById('canvas');
+let newGame = document.getElementById('nuevo-juego');
+let cancelar = document.getElementById('cancelar');
+let salir = document.getElementById('salir');
 
+let tablero  = document.getElementById('canvas').getContext('2d');
+let palabras = ['ALURA','ORACLE','ONE','JAVASCRIPT','HTML'];
+let palabraSecreta = '';
+let palabraCorrecta = '';
+let letras = [];
+let letrasIncorrectas = [];
+let letraElegida = [];
+let errores = 8;
+let numErrores = 8;
+
+//Mostrar text-area
+const mostrarAgregarPalabra = () => {
+    document.getElementById('iniciar-juego').style.display = 'none';
+    document.getElementById('nueva-palabra').style.display = 'none';
+    document.getElementById('n-palabra').style.display = 'flex';
+    document.getElementById('texto').style.display = 'flex';
+    document.getElementById('button-5-6-content').style.display = 'flex';
+}
+
+document.getElementById('iniciar-juego').onclick = () => {
+    iniciarJuego();
+};
+newGame.addEventListener('click', () => {
+    location.reload();
+});
+cancelar.addEventListener('click', () => {
+    location.reload();
+});
+salir.addEventListener('click', () => {
+    location.reload();
+});
+document.getElementById('guardar-empezar').onclick = () => {
+    nuevaPalabra();
+};
+
+//Verificar si el juego termino
+const finJuego = (letra) => {
+    if(letraElegida.length < palabraSecreta.length) {
+        letrasIncorrectas.push(letra);
+        if(letrasIncorrectas.length > numErrores) {
+            perdio();
+        }else if (letraElegida.length < palabraSecreta.length) {
+            agragarLetraIncorrecta(letra);
+            letraIncorrecta(letra,errores);
+        }
+    }
+}
+//Verifico si ganó
+const juegoGanado = (letra) => {
+    letraElegida.push(letra.toUpperCase());
+    if(letraElegida.length == palabraSecreta.length) {
+        gano();
+    }
+}
 //Palabra secreta 
 const buscarPalabraSecreta = () => {
     let palabra = palabras[Math.floor(Math.random() * palabras.length)];
     palabraSecreta = palabra;
     console.log(palabraSecreta);
 }
-//Validar la letra ingresada
-const validarLetra = (key) => {
-    //Entre 65 y 90 estan todas las mayusculas ASCII
-    if(key >= 65 && letras.indexOf(key) || 
-       key <= 90 && letras.indexOf(key)) {
+//Verifico la letra ingresada
+const letraIngresada = (key) => {
+    if(letras.length < 1 || letras.indexOf(key) < 0) {
         letras.push(key);
-        console.log(key);
         return false;
-    } else {
-        console.log(key);
+    }else {
+        letras.push(key);
         return true;
     }
 }
-//AgragarLetraIncorrecta
-const agragarLetraIncorrecta = () => {
-    errores -= 1; 
-    console.log(errores)
+//Teclas que no sean válidas
+const verificarLetra = (keyCode) => {
+    if(typeof keyCode === 'number' && keyCode >= 65 && keyCode <= 90) {
+        return true;
+    }else {
+        return false;
+    }
 }
-
+//Agragar letra Correcta
+const agragarLetraCorrecta = (i) => {
+    palabraCorrecta += palabraSecreta[i].toUpperCase();
+}
+//AgragarLetraIncorrecta
+const agragarLetraIncorrecta = (aLetra) => {
+    if(palabraSecreta.indexOf(aLetra) <= 0) {
+        errores -= 1; 
+    }
+}
+//Gaurdar nueva palabra 
+const nuevaPalabra = () => {
+    let nuevaPalabra = document.getElementById('n-palabra').value;
+    if(nuevaPalabra !== '') {
+        palabras.push(nuevaPalabra.toUpperCase());
+        alert('Palabra guardada...');
+        document.getElementById('n-palabra').style.display = 'none';
+        document.getElementById('texto').style.display = 'none';
+        document.getElementById('button-5-6-content').style.display = 'none';
+        iniciarJuego();
+    } else {
+        alert('Debes ingresar una palabra :( ');
+    }
+}
 //Inicio del Juego
 const iniciarJuego = () => {
-    document.getElementById('uno').style.display = 'none';
-    document.getElementById('dos').style.display = 'none';
+    document.getElementById('iniciar-juego').style.display = 'none';
+    document.getElementById('nueva-palabra').style.display = 'none';
+    document.getElementById('salir').style.display = 'block';
+    document.getElementById('nuevo-juego').style.display = 'block';
     buscarPalabraSecreta();
     crearCanvas();
     dibujarLineas();
-
+//Que letra se presionó 
     document.onkeydown = (ev) => {
         let letra = ev.key.toUpperCase();
-        if(validarLetra(letra) && palabraSecreta.includes(letra)) {
-            for(let i = 0; i < palabraSecreta.length; i++) {
-                if(palabraSecreta[i] === letra) {
-                    letraCorrecta(i);
+        if(letrasIncorrectas.length <= numErrores) {
+            if(!letraIngresada(ev.key) && verificarLetra(ev.keyCode)){
+                if(palabraSecreta.includes(letra)) {
+                    agragarLetraCorrecta(palabraSecreta.indexOf(letra))
+                    for(let i = 0; i < palabraSecreta.length; i++) {
+                        if(palabraSecreta[i] === letra) {
+                            letraCorrecta(i);
+                            juegoGanado(letra);
+                        }
+                    }
+                }else {
+                    if(!letraIngresada(ev.key) && !juegoGanado(letra)) return;
+                        dibujarAhorcado(errores);
+                        finJuego(letra);
                 }
             }
         }else {
-            agragarLetraIncorrecta(letra);
-            letraIncorrecta(letra, errores);
+            alert('Ups! llegaste al limite de letras incorrectas :( ');
         }
-    }
+    };
 }
 
